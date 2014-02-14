8421ff8cb452fb7c6717391b231729093959e328
Javran Cheng <Javran.c@gmail.com>
2014-02-14 00:41:35 -0500

    bump recent changes

--- init.el
+++ init.el
@@ -8,25 +8,15 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(TeX-PDF-mode t)
  '(blink-cursor-mode nil)
  '(custom-enabled-themes (quote (manoj-dark)))
- '(custom-safe-themes (quote ("e16a771a13a202ee6e276d06098bc77f008b73bbac4d526f160faa2d76c1dd0e"
-			      "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4"
-			      default)))
+ '(custom-safe-themes (quote ("e16a771a13a202ee6e276d06098bc77f008b73bbac4d526f160faa2d76c1dd0e" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
  '(global-linum-mode t)
  '(haskell-indent-spaces 4)
  '(menu-bar-mode nil)
- '(package-archives (quote (("melpa" . "http://melpa.milkbox.net/packages/")
-			    ("gnu" . "http://elpa.gnu.org/packages/"))))
- '(quack-programs (quote ("mitscheme" "racket" "bigloo"
-			  "csi" "csi -hygienic" "gosh"
-			  "gsi" "gsi ~~/syntax-case.scm -"
-			  "guile" "kawa" "mit-scheme" "mred -z"
-			  "mzscheme" "mzscheme -il r6rs"
-			  "mzscheme -il typed-scheme"
-			  "mzscheme -M errortrace"
-			  "mzscheme3m" "mzschemecgc" "rs"
-			  "scheme" "scheme48" "scsh" "sisc" "stklos" "sxi")))
+ '(package-archives (quote (("melpa" . "http://melpa.milkbox.net/packages/") ("gnu" . "http://elpa.gnu.org/packages/"))))
+ '(quack-programs (quote ("mitscheme" "racket" "bigloo" "csi" "csi -hygienic" "gosh" "gsi" "gsi ~~/syntax-case.scm -" "guile" "kawa" "mit-scheme" "mred -z" "mzscheme" "mzscheme -il r6rs" "mzscheme -il typed-scheme" "mzscheme -M errortrace" "mzscheme3m" "mzschemecgc" "rs" "scheme" "scheme48" "scsh" "sisc" "stklos" "sxi")))
  '(scroll-bar-mode nil)
  '(tool-bar-mode nil))
 (custom-set-faces
@@ -34,19 +24,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil
-		:stipple nil
-		:inverse-video nil
-		:box nil
-		:strike-through nil
-		:overline nil
-		:underline nil
-		:slant normal
-		:weight normal
-		:height 120
-		:width normal
-		:foundry "unknown"
-		:family "DejaVu Sans Mono")))))
+ '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "unknown" :family "DejaVu Sans Mono")))))
 
 ;; required by ghc mode
 ;;   using flymake
@@ -71,7 +49,7 @@
 (defun newline-before-current-line ()
   "start a new line before the current line
    and move cursor there"
-  (interactive) 
+  (interactive)
   (beginning-of-line)
   (newline)
   (previous-line))
@@ -105,7 +83,7 @@ vi style of % jumping to matching brace."
          (newmap (or (cdr (assoc mode minor-mode-overriding-map-alist))
                      (let ((map (make-sparse-keymap)))
                        (set-keymap-parent map oldmap)
-                       (push `(,mode . ,map) minor-mode-overriding-map-alist) 
+                       (push `(,mode . ,map) minor-mode-overriding-map-alist)
                        map))))
     (define-key newmap key def)))
 
@@ -126,3 +104,11 @@ vi style of % jumping to matching brace."
 (add-hook 'haskell-mode-hook
 	  'haskell-indentation-mode)
 
+(setq make-backup-files nil)
+
+(add-hook 'LaTeX-mode-hook
+          (lambda ()
+            (setq TeX-engine 'xetex)))
+
+(setq ac-ignore-case nil)
+(setq-default show-trailing-whitespace t)
