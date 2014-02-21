f72fe519d51972a99c7c50dbf7b224f7d960bcbb
Javran Cheng <Javran.c@gmail.com>
2014-02-21 14:58:10 -0500

    todo - auto-complete functionality?
    do we have some keybinding to select prev/next candidate

--- init.el
+++ init.el
@@ -7,10 +7,11 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(TeX-PDF-mode t)
+ '(ac-auto-show-menu t)
  '(ac-modes (quote (markdown-mode emacs-lisp-mode lisp-mode lisp-interaction-mode slime-repl-mode c-mode cc-mode c++-mode go-mode java-mode malabar-mode clojure-mode clojurescript-mode scala-mode scheme-mode ocaml-mode tuareg-mode coq-mode haskell-mode agda-mode agda2-mode perl-mode cperl-mode python-mode ruby-mode lua-mode ecmascript-mode javascript-mode js-mode js2-mode php-mode css-mode makefile-mode sh-mode fortran-mode f90-mode ada-mode xml-mode sgml-mode ts-mode sclang-mode verilog-mode)))
+ '(blink-cursor-mode nil)
  '(custom-enabled-themes (quote (manoj-dark)))
  '(custom-safe-themes (quote ("e16a771a13a202ee6e276d06098bc77f008b73bbac4d526f160faa2d76c1dd0e" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
- '(blink-cursor-mode nil)
  '(global-linum-mode t)
  '(haskell-indent-spaces 4)
  '(hl-sexp-background-color "RoyalBlue4")
