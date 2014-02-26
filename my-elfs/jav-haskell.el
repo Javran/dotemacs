565ceb92865faf877749a9bddefe2bc95b107257
Javran Cheng <Javran.c@gmail.com>
2014-02-26 13:04:34 -0500

    tweak haskell-mode
    * for most of the places, use 4-sp-indent
    * hlint

--- my-elfs/jav-haskell.el
+++ my-elfs/jav-haskell.el
@@ -3,9 +3,31 @@
 ;; required by ghc mode
 ;;   using flymake
 (autoload 'ghc-init "ghc" nil t)
-(add-hook 'haskell-mode-hook (lambda () (ghc-init) (flymake-mode)))
 
-(add-hook 'haskell-mode-hook
-	  'turn-on-haskell-indent)
+(setq haskell-indent-spaces 4)
 
+(setq haskell-indent-after-keywords
+      (quote (("where" 4 0)
+              ("of" 2)
+              ("do" 4)
+              ("mdo" 4)
+              ("rec" 2)
+              ("in" 4 0)
+              ("{" 2)
+              "if"
+              "then"
+              "else"
+              "let")))
+
+(defun my-haskell-mode-hook ()
+  "customizations for haskell mode"
+  (ghc-init)
+  (flymake-mode)
+  (haskell-indentation-mode -1)
+  (turn-on-haskell-indent)
+  (setq locale-coding-system 'utf-8)
+  (flymake-haskell-multi-load)
+  )
+
+(add-hook 'haskell-mode-hook 'my-haskell-mode-hook)
 (provide 'jav-haskell)
