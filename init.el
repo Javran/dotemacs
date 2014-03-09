e0c82f5ec153ff9b14f6e0af7608e9315c036610
Javran Cheng <Javran.c@gmail.com>
2014-03-09 03:53:00 -0400

    change theme & font

--- init.el
+++ init.el
@@ -26,13 +26,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector ["black" "#d55e00" "#009e73" "#f8ec59" "#0072b2" "#cc79a7" "#56b4e9" "white"])
  '(custom-enabled-themes (quote (manoj-dark)))
  '(custom-safe-themes (quote ("e16a771a13a202ee6e276d06098bc77f008b73bbac4d526f160faa2d76c1dd0e" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
- '(haskell-indent-after-keywords (quote (("where" 4 0) ("of" 2) ("do" 4) ("mdo" 4) ("rec" 2) ("in" 4 0) ("{" 2) "if" "then" "else" "let")))
+ '(haskell-indent-after-keywords (quote (("where" 4 0) ("of" 2) ("do" 4) ("mdo" 4) ("rec" 2) ("in" 4 0) ("{" 2) "if" "then" "else" "let")) t)
  '(package-archives (quote (("melpa" . "http://melpa.milkbox.net/packages/") ("gnu" . "http://elpa.gnu.org/packages/")))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "unknown" :family "DejaVu Sans Mono")))))
+ '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "unknown" :family "Luculent")))))
